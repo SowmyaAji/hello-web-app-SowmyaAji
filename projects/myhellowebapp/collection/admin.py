fe96918a11aed4892aa177259a3251812547af32
Sowmya <sowmya.aji@gmail.com>
2018-11-14 17:44:19 -0500

    book website called knits

--- projects/myhellowebapp/collection/admin.py
+++ projects/myhellowebapp/collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from collection.models import Knits
+
+
+class KnitsAdmin(admin.ModelAdmin):
+    model = Knits
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Knits, KnitsAdmin)
