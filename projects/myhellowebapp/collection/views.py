3d4bf391f4bd61873b1941ac0180bfc0c81d05fa
Sowmya <sowmya.aji@gmail.com>
2018-11-14 14:50:27 -0500

    name changes

--- projects/myhellowebapp/collection/views.py
+++ projects/myhellowebapp/collection/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 # this is your new view
-    return render(request, 'index.html')
+
+    number = 7
+
+    thing = "Alam Aara"
+
+    return render(request, 'index.html', {
+        'number': number,
+        'thing': thing,
+        })
